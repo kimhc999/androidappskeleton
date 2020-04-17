aec5d33fb158bcf28d7820adcdfc01cef092e457
Eddy Hyunchul Kim <kimhc999@hotmail.com>
2020-04-17 21:35:54 +0900

    add reference site

--- app/src/main/java/com/example/androidappskeleton/utils/Event.kt
+++ app/src/main/java/com/example/androidappskeleton/utils/Event.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * https://github.com/google/iosched/blob/2696fc7e06826ba2db72de243f0d63f83f4a29b5/shared/src/main/java/com/google/samples/apps/iosched/shared/result/Event.kt
+ */
+
 package com.example.androidappskeleton.utils
 
 import androidx.lifecycle.Observer
